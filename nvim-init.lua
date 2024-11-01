dd02dec2912c8a49afedbc9ba30c32e84524fcb1
Zachary Milonas <25948390+zmilonas@users.noreply.github.com>
2024-11-01 14:23:32 +0100

    Update nvim settings

--- nvim-init.lua
+++ nvim-init.lua
@@ -1,89 +1,87 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 vim.opt.directory = vim.fn.expand("$HOME/.local/share/nvim/swap")
 
+vim.g.mapleader = " "
 
--- Example using a list of specs with the default options
-vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-require('lazy').setup {
+require("lazy").setup({
 
   -- git related
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
 
   -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  "tpope/vim-sleuth",
 
   {
     -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
 
       -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim', opts = {} },
+      { "j-hui/fidget.nvim", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+      "folke/neodev.nvim",
     },
   },
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "navarasu/onedark.nvim",
     priority = 1000,
     lazy = false,
     config = function()
-      require('onedark').setup {
+      require("onedark").setup({
         -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-      }
-      require('onedark').load()
+        style = "dark", -- dark, darker, cool, deep, warm, warmer, light
+      })
+      require("onedark").load()
     end,
   },
 
-  { 'folke/which-key.nvim', opts = {} },
+  { "folke/which-key.nvim", opts = {} },
 
-  { 'folke/neoconf.nvim', cmd = 'Neoconf' },
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
 
   {
     -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
-    main = 'ibl',
+    main = "ibl",
     opts = {},
   },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
+        "nvim-telescope/telescope-fzf-native.nvim",
         -- NOTE: If you are having trouble with this installation,
         --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
+        build = "make",
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable("make") == 1
         end,
       },
     },
@@ -91,188 +89,261 @@ require('lazy').setup {
 
   {
     -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    build = ':TSUpdate',
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "php",
+        "html",
+        "javascript",
+        "python",
+      },
+      highlight = {
+        enable = true,
+      },
+    },
   },
 
-  'folke/neodev.nvim',
+  -- Laravel Blade templates
+  {
+    "jwalton512/vim-blade",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = "blade",
+  },
 
-  'hrsh7th/cmp-nvim-lsp',
+  "folke/neodev.nvim",
 
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'MunifTanjim/nui.nvim',
+  "MunifTanjim/nui.nvim",
 
   {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
-        '<leader>ff',
+        "<leader>p",
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require("conform").format({ async = false, lsp_fallback = true })
         end,
-        mode = '',
-        desc = 'Format buffer',
+        mode = "",
+        desc = "Format file",
       },
     },
     opts = {
+      formatters = {
+        stylua = {
+          -- Stylua specific configuration for smaller indents
+          prepend_args = {
+            "--indent-type",
+            "Spaces",
+            "--indent-width",
+            "2",
+            "--column-width",
+            "120",
+          },
+        },
+      },
       formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        javascript = { { 'prettierd', 'prettier' } },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
       },
     },
   },
+
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
 
   {
     -- Autocompletion
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
-          if vim.fn.has 'win32' == 1 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+          completion = {
+            completeopt = "menu,menuone,noinsert",
+          },
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+          },
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "path" },
+            { name = "buffer" },
+          }),
+        })
+      end,
     },
   },
 
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    cmd = 'Neotree',
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
     },
+
     keys = {
       {
-        '<leader>e',
+        "<leader>E",
         function()
-          require('neo-tree.command').execute { toggle = true, dir = Util.root() }
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
         end,
-        desc = 'Explorer NeoTree (root dir)',
+        desc = "Explorer NeoTree (cwd)",
       },
       {
-        '<leader>E',
+        "<leader>g",
         function()
-          require('neo-tree.command').execute { toggle = true, dir = vim.loop.cwd() }
+          require("neo-tree.command").execute({ source = "git_status", toggle = true })
         end,
-        desc = 'Explorer NeoTree (cwd)',
+        desc = "Git explorer",
       },
       {
-        '<leader>g',
+        "<leader>b",
         function()
-          require('neo-tree.command').execute { source = 'git_status', toggle = true }
+          require("neo-tree.command").execute({ source = "buffers", toggle = true })
         end,
-        desc = 'Git explorer',
-      },
-      {
-        '<leader>b',
-        function()
-          require('neo-tree.command').execute { source = 'buffers', toggle = true }
-        end,
-        desc = 'Buffer explorer',
+        desc = "Buffer explorer",
       },
     },
+
     deactivate = function()
-      vim.cmd [[Neotree close]]
+      vim.cmd([[Neotree close]])
     end,
     init = function()
       if vim.fn.argc(-1) == 1 then
         local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == 'directory' then
-          require 'neo-tree'
+        if stat and stat.type == "directory" then
+          require("neo-tree")
         end
       end
     end,
+
     opts = {
-      sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
-      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+      close_if_last_window = false,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        window = {
+          mappings = {
+            ["Y"] = function(state)
+              -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+              -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+              local node = state.tree:get_node()
+              local filepath = node:get_id()
+              local filename = node.name
+              local modify = vim.fn.fnamemodify
+
+              local results = {
+                filepath,
+                modify(filepath, ":."),
+                modify(filepath, ":~"),
+                filename,
+                modify(filename, ":r"),
+                modify(filename, ":e"),
+              }
+
+              local messages = {
+                "Choose to copy to clipboard:",
+                "1. Absolute path: " .. results[1],
+                "2. Path relative to CWD: " .. results[2],
+                "3. Path relative to HOME: " .. results[3],
+                "4. Filename: " .. results[4],
+                "5. Filename without extension: " .. results[5],
+                "6. Extension of the filename: " .. results[6],
+              }
+              vim.api.nvim_echo({ { table.concat(messages, "\n"), "Normal" } }, true, {})
+              local i = vim.fn.getchar()
+
+              if i >= 49 and i <= 54 then
+                local result = results[i - 48]
+                print(result)
+                vim.fn.setreg("*", result)
+              else
+                print("Invalid choice: " .. string.char(i))
+              end
+            end,
+          },
+        },
       },
       window = {
         mappings = {
-          ['<space>'] = 'none',
-          ['Y'] = function(state)
+          ["<space>"] = "none",
+          ["Y"] = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
-            vim.fn.setreg('+', path, 'c')
+            vim.fn.setreg("+", path, "c")
           end,
         },
       },
       default_component_configs = {
         indent = {
           with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = '',
-          expander_expanded = '',
-          expander_highlight = 'NeoTreeExpander',
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
         },
       },
     },
-    config = function(_, opts)
-      local function on_move(data)
-        Util.lsp.on_rename(data.source, data.destination)
-      end
 
-      local events = require 'neo-tree.events'
+    config = function(_, opts)
+      local events = require("neo-tree.events")
       opts.event_handlers = opts.event_handlers or {}
-      vim.list_extend(opts.event_handlers, {
-        { event = events.FILE_MOVED, handler = on_move },
-        { event = events.FILE_RENAMED, handler = on_move },
-      })
-      require('neo-tree').setup(opts)
-      vim.api.nvim_create_autocmd('TermClose', {
-        pattern = '*lazygit',
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
         callback = function()
-          if package.loaded['neo-tree.sources.git_status'] then
-            require('neo-tree.sources.git_status').refresh()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
           end
         end,
       })
     end,
   },
-}
+})
 
-vim.api.nvim_create_user_command('Format', function(args)
+vim.api.nvim_create_user_command("Format", function(args)
   local range = nil
   if args.count ~= -1 then
     local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
     range = {
       start = { args.line1, 0 },
-      ['end'] = { args.line2, end_line:len() },
+      ["end"] = { args.line2, end_line:len() },
     }
   end
-  require('conform').format { async = true, lsp_fallback = true, range = range }
+  require("conform").format({ async = true, lsp_fallback = true, range = range })
 end, { range = true })
 
 -- Make line numbers default
 vim.wo.number = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -285,29 +356,44 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 vim.o.termguicolors = true
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require("mason-lspconfig").setup_handlers({
+  -- auto setup of lsp servers
+  function(server_name)
+    require("lspconfig")[server_name].setup({
+      capabilities = capabilities,
+    })
+  end,
+})
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "ts_ls",
+    "eslint",
+    "pyright",
+    "intelephense",
+    "docker_compose_language_service",
+    "groovyls",
+    "dockerls",
+    "jsonls",
+    "html",
+  },
+  automatic_installation = true,
+})
 
-require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
-    end,
-}
+local lspconfig = require("lspconfig")
 
-
-
-require('lspconfig').groovyls.setup {
+lspconfig.groovyls.setup({
   filetypes = { "groovy", "jenkinsfile" },
-  root_dir = require('lspconfig').util.root_pattern(".git", "*.gradle", "Jenkinsfile"),
-}
+  root_dir = lspconfig.util.root_pattern(".git", "*.gradle", "Jenkinsfile"),
+})
 
 -- Ensure Jenkinsfiles are recognized as Groovy files
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
@@ -318,28 +404,63 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 })
 
 -- TELESCOPE Config
-require('telescope').setup {
+require("telescope").setup({
   defaults = {
+    -- Default configuration for all pickers
+    file_ignore_patterns = {
+      "node_modules/",
+      ".git/",
+      "target/",
+      "build/",
+      "dist/",
+      "vendor/",
+      "%.lock",
+    },
     mappings = {
       i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ["<C-u>"] = false,
+        ["<C-d>"] = false,
       },
     },
   },
-}
+  pickers = {
+    find_files = {
+      -- This is the JetBrains behavior - showing hidden files but respecting gitignore
+      hidden = true,
+      respect_gitignore = true,
+      no_ignore = false,
+    },
+  },
+})
 
+-- ZACHARY KEYMAPS!!!
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>?", telescope.oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", telescope.buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>/", function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  telescope.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
     winblend = 10,
     previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
+  }))
+end, { desc = "[/] Fuzzily search in current buffer" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- JetBrains-like keybindings
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Go to declaration" })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Go to definition" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "Hover documentation" })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf, desc = "Go to implementation" })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename symbol" })
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Code action" })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, desc = "Go to references" })
+  end,
+})
